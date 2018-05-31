c3a42c4f4e36b5d947ac8a81764468cd5febf7f4
liyasthomas <liyascthomas@gmail.com>
2018-05-31 22:31:57 +0530

    Initial changes

--- app/src/main/java/at/xtools/pwawrapper/Constants.java
+++ app/src/main/java/at/xtools/pwawrapper/Constants.java
@@ -3,8 +3,8 @@ package at.xtools.pwawrapper;
 public class Constants {
     public Constants(){}
     // Root page
-    public static String WEBAPP_URL = "https://www.leasingrechnen.at/";
-    public static String WEBAPP_HOST = "leasingrechnen.at"; // used for checking Intent-URLs
+    public static String WEBAPP_URL = "https://liyasthomas.github.io/lvr/";
+    public static String WEBAPP_HOST = "liyasthomas.github.io/lvr"; // used for checking Intent-URLs
 
 	// User Agent tweaks
     public static boolean POSTFIX_USER_AGENT = true; // set to true to append USER_AGENT_POSTFIX to user agent
